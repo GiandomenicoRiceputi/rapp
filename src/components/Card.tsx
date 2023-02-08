f2a204d13ea28e665d01c0a6271b8b0362ca2549
giandodev <giandodev@giandodevs-Mac-Studio.local>
2023-02-08 21:07:39 +0100

    refactor

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,14 @@
+
 import React, { useState, useEffect } from 'react';
 import './card.css'
-
 interface UserData {
     results: UserData[];
 }
-
 const Card: React.FC = () => {
     const [userData, setUserData] = useState<UserData>();
-    const [time, setTime] = useState(new Date().toLocaleTimeString());
-    const [day, setDay] = useState(new Date().toLocaleDateString());
-
+    const date = new Date();
+    const [time, setTime] = useState(date.toLocaleTimeString());
+    const [day, setDay] = useState(date.toLocaleDateString());
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch('https://randomuser.me/api/?results=20');
@@ -19,11 +18,8 @@ const Card: React.FC = () => {
         fetchData();
     }, []);
 
-    console.log(userData)
-
     useEffect(() => {
         const intervalId = setInterval(() => {
-            const date = new Date();
             setTime(
                 date.toLocaleTimeString(undefined, {
                     hour: '2-digit',
@@ -37,34 +33,26 @@ const Card: React.FC = () => {
                 })
             );
         }, 1000);
-
         return () => clearInterval(intervalId);
     }, []);
-
     if (!userData) {
         return <p>Loading...</p>;
     }
-
-
-
     return (
         <>
             {userData?.results.map((user) => (
-
                 <div className="card" key={user.login.uuid}>
                     <img src={user.picture.medium} alt="Profile Picture" />
                     <div className="card-text">
                         <p className="card-name">
-                          Name:  {user.name.first} {user.name.last}
+                            Name:  {user.name.first} {user.name.last}
                         </p>
                         <p className="card-date">Date: {day} {' '} {time}</p>
                         <p className="card-description">Username: {user.login.username}</p>
                     </div>
                 </div>
             ))}
-    </>
-
+        </>
     );
 };
-
 export default Card;
